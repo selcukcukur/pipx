680a9e1b93925644dccb8bb0c75458de6e19e1c4
selcukcukur <selcukcukur@outlook.com.tr>
2026-06-16 07:56:18 +0300

    feat(core): add async steps macro

--- crates/pipx/src/macros.rs
+++ crates/pipx/src/macros.rs
@@ -15,3 +15,22 @@ macro_rules! steps {
         ]
     }};
 }
+
+/// Creates a list of asynchronous pipeline steps.
+///
+/// **Parameters**
+/// - `$pipe` - One or more async pipe expressions that should be stored as asynchronous pipeline steps.
+///
+/// **Returns**
+/// - [`Vec`] - A vector containing the provided steps as [`AsyncPipelineStep`] values.
+#[cfg(feature = "async")]
+#[macro_export]
+macro_rules! async_steps {
+    ($($pipe:expr),+ $(,)?) => {{
+        vec![
+            $(
+                std::sync::Arc::new($pipe) as $crate::AsyncPipelineStep<_, _>
+            ),+
+        ]
+    }};
+}
