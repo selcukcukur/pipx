6f985ab6894d1e8eda736bc43421cc5d85c4790c
selcukcukur <selcukcukur@outlook.com.tr>
2026-06-16 04:07:07 +0300

    feat(core): add steps macro documentation

--- crates/pipx/src/macros.rs
+++ crates/pipx/src/macros.rs
@@ -1,3 +1,10 @@
+/// Creates a list of pipeline steps.
+///
+/// **Parameters**
+/// - `$pipe` - One or more pipe expressions that should be stored as pipeline steps.
+///
+/// **Returns**
+/// - [`Vec`] - A vector containing the provided steps as [`PipelineStep`] values.
 #[macro_export]
 macro_rules! steps {
     ($($pipe:expr),+ $(,)?) => {{
